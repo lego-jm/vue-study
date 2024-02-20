be8627eb235bbf1c6991f973bc301399b7762499
lego_jm <wjddb10@gmail.com>
2024-02-20 22:41:01 +0900

    'test'

---
+++ vue3-app/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <AppHeader></AppHeader>
+  <div>{{ message }}</div>
+  <button @click="showAlert">경고</button>
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  components: {
+    AppHeader,
+  },
+  data() {
+    return {
+      message: "hi",
+    };
+  },
+  methods: {
+    showAlert() {
+      alert("hello");
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ vue3-app/src/components/AppHeader.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1>앱 헤더</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped></style>
